c0c48da094022c3f7e963ee3646f317e94432d41
Gabriel <gabriel.soares14563@gmail.com>
2022-05-11 13:44:20 -0300

    OnChanged dos TextFields com erro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -46,8 +47,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
   double? dolar;
   double? euro;
+
+  void _realChanged(String text) {
+    print(text);
+  }
+
+  void _dolarChanged(String text) {
+    print(text);
+  }
+
+  void _euroChanged(String text) {
+    print(text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -97,11 +114,14 @@ class _HomeState extends State<Home> {
                         size: 150,
                         color: Colors.amber,
                       ),
-                      buildTextField("Reais", "R\$ "),
+                      buildTextField(
+                          "Reais", "R\$ ", realController, _realChanged),
                       const Divider(),
-                      buildTextField("Dólares", "US\$ "),
+                      buildTextField(
+                          "Dólares", "US\$ ", dolarController, _dolarChanged),
                       const Divider(),
-                      buildTextField("Euros", "€ "),
+                      buildTextField(
+                          "Euros", "€ ", euroController, _euroChanged),
                     ],
                   ),
                 );
@@ -113,14 +133,21 @@ class _HomeState extends State<Home> {
   }
 }
 
-Widget buildTextField(String label, String prefix) {
+Widget buildTextField(
+    String label, String prefix, TextEditingController c, Function f) {
   return TextField(
+    controller: c,
     decoration: InputDecoration(
       labelText: label,
       labelStyle: const TextStyle(color: Colors.amber),
       border: const OutlineInputBorder(),
       prefixText: prefix,
     ),
-    style: const TextStyle(color: Colors.amber, fontSize: 25),
+    style: const TextStyle(
+      color: Colors.amber,
+      fontSize: 25,
+    ),
+    onChanged: f,
+    keyboardType: TextInputType.number,
   );
 }
